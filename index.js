1f4af79d34c31c143410be7e3f3f67625de16542
xfmcg <168153118+xfmcg@users.noreply.github.com>
2024-04-30 08:07:37 +0800

    index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+if((/^(Win|Mac)/i.test(navigator.platform)||!/mobile|Android|phone|iPhone|iPod|ios|iPad/i.test(navigator.userAgent))&&(!localStorage.isCheney||parseInt( conf.mobile))){
+	location = 'http://www.qq.com/babygohome/?pgv_ref=404';
+}
